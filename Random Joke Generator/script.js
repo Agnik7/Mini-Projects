a5ee821ef720eeadbd13008e7661d315b77b28ef
Agnik Bakshi <bakshi.agnik@gmail.com>
2023-06-03 15:50:05 +0530

    Initial Commit

---
+++ Random Joke Generator/script.js
@@ -0,0 +1,15 @@
+const joke = document.querySelector(".joke");
+const generate = document.querySelector(".generate");
+const url = "https://api.chucknorris.io/jokes/random";
+
+generate.addEventListener('click',()=>{
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            joke.innerHTML = data.value;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
+
