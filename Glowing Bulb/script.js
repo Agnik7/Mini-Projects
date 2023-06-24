2c5c3aa769a4751eaa8cdbfb84565068c8fd54ee
Agnik Bakshi <bakshi.agnik@gmail.com>
2023-06-24 10:57:22 +0530

    Initial Commit

---
+++ Glowing Bulb/script.js
@@ -0,0 +1,21 @@
+const button = document.querySelector("button");
+const state = document.querySelector(".on-off");
+const bulb = document.querySelector("#bulb");
+let flag = 0; // flag=0 --> Off |||||| flag=1 --> On
+
+button.addEventListener('click',()=>{
+    if(flag == 0)
+    {
+        state.innerHTML = 'Off';
+        flag = 1;
+        bulb.style.color = "var(--yellow)";
+        bulb.style.filter = "drop-shadow(0 0 16px var(--yellow))";
+    }
+    else
+    {
+        state.innerHTML = 'On';
+        flag = 0;
+        bulb.style.color = "white";
+        bulb.style.filter = "none";
+    }
+});
